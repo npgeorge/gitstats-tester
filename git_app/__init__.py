22c759fc8596704f3396f1d8cc5247edae55fbf9
Nick George <53956922+npgeorge@users.noreply.github.com>
2020-06-01 23:21:33 -0400

    updated init file

--- git_app/__init__.py
+++ git_app/__init__.py
@@ -4,7 +4,7 @@ from git_app.app import create_app
 # if we run the init file, it should run our app after importing it
 if __name__ =="__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run_server(debug=True)
 
 #try this next
 #if __name__ == '__main__':

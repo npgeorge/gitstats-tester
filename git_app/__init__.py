7eca9ac40902851632d56d9ba5da2a949254f833
Nick George <53956922+npgeorge@users.noreply.github.com>
2020-06-02 10:23:22 -0400

    updated procfile

--- git_app/__init__.py
+++ git_app/__init__.py
@@ -1,8 +1,6 @@
 # import the create_app function from our app file
 from git_app.app import create_app
 
-#app = create_app()
-
 # if we run the init file, it should run our app after importing it
 if __name__ =="__main__":
     app = create_app()
